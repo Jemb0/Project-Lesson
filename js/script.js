e6b21d11dde4fd24edeeda47d2ea5ebb9ce2c863
Kirill <jembo@internet.ru>
2021-06-14 16:35:58 +0600

    Second lesson

--- js/script.js
+++ js/script.js
@@ -21,3 +21,38 @@ personalMovieDB.movies [a] = b;
 personalMovieDB.movies [c] = d;
 
 console.log(personalMovieDB);
+
+if (4 == 3) {
+    console.log('Три равно трём');
+} else {
+    console.log('Error');
+}
+
+const number = 667
+
+if (number > 668) {
+    console.log('Поезр');
+} else if (number < 666) {
+    console.log('Морген чтоли');
+} else {
+    console.log('Гуд!');
+}
+
+(number === 50) ? console.log('Гуд') : console.log('Error');
+
+const num = 1232
+
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100: 
+        console.log('Неверно');
+        break;
+    case 12:
+        console.log('Гуд');
+        break;
+    default:
+        console.log('не в этот раз');
+        break;
+}
